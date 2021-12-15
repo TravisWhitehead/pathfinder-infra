18c9ba47227364891d5832457d7650e956a5e48e
Travis Whitehead <traviswhitehead@hey.com>
2021-12-14 20:40:39 -0800

    Add HTTP(S) ingress security group roles

--- index.ts
+++ index.ts
@@ -51,6 +51,8 @@ const ami = pulumi.output(aws.ec2.getAmi({
 const security_group = new aws.ec2.SecurityGroup("web-sg", {
     ingress: [
         { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
+        { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
+        { protocol: "tcp", fromPort: 443, toPort: 443, cidrBlocks: ["0.0.0.0/0"] },
     ],
     egress: [
         { protocol: "-1", fromPort: 0, toPort: 0, cidrBlocks: ["0.0.0.0/0"] },
